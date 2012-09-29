f4ed6a9cbc92a2b7d698961f5a659cac0720107f
Andy <virtualandy@gmail.com>
2012-09-29 18:33:05 -0400

    barely working

--- plugins/denver/assets/js/client.js
+++ plugins/denver/assets/js/client.js
@@ -1,16 +1,58 @@
-var socket = io.connect();
+var socket = io.connect(); 
 
-socket.on("connect", function() {
-    $("#chat_input").removeAttr("disabled");
-});
-
-socket.on("notice", function(msg) {
-    // Notices (user join, user leave...)
-    alert(msg.type);
-});
-
-socket.on("chat", function(msg) {
-    // Output this somewhere. I wonder where!
-});
+(function($) {
+    
+    socket.on("connect", function() {
+        console.log('connected');
+        $("#chat_input").removeAttr("disabled");
+    });
+    
+    socket.on("notice", function(msg) {
+        // Notices (user join, user leave...)
+        console.log('user joined ',msg);
+        $("#chat_output_container").append(msg.msg);
+    });
+    
+    socket.on("chat", function(msg) {
+        // Output this somewhere. I wonder where!
+        console.log('got a message ',msg.msg);
+        $("#chat_output_container").append(msg.msg);
+        //console.log($("#chat_input").value);
+    });
+    
+    socket.on("newloc", function(data) {
+        console.log("new location: ",data);
+        var myFrame = $('#wheredat');
+        var url = "http://wheredat.azurewebsites.net/" + '?address=' + data.address;
+        $(myFrame).attr('src', url);
+    });
+    
+    $("#chat_output_container").text("Where did you hide the XBox?");
+    var inputVal = "";
+    console.log('jquery loaded');
+    
+    $('#chat_input_container').keypress(function (e) {
+        e.preventDefault();
+        if (e.which == 13) {
+            console.log('enter pressed');
+            socket.emit("chat", $("#chat_input").val);
+        }
+    });
+    
+    /*$('#chat_input').keyup(function(){
+        this.preventDefault();
+        console.log($('#chat_input').val);
+    });
+    
+    $("#chat_input").bind('keypress', function () {
+          inputVal += $(this).val();
+          console.log('key up ',inputVal);
+    }).bind('keyup', function() {
+        console.log('emitting ',inputVal);
+        socket.emit("chat", inputVal);
+        inputVal = "";
+    });*/
+    
+})(jQuery);
 
 // To send a message, socket.emit("chat", chat_input.value);
--- plugins/denver/denver.js
+++ plugins/denver/denver.js
@@ -21,11 +21,30 @@ io.configure(function() {
     io.set("log level", 1);
 });
 
+var AlchemyAPI = require('alchemy-api');
+var alchemy = new AlchemyAPI('ff498607b371672bb7fc60a1d736d0e66e0266e9');
+
+function getAlchemy(inputText) {
+    console.log("paging alchemy for info on ",inputText);
+    alchemy.entities(inputText, {}, function(err, response) {
+      if (err) throw err;
+    
+      // See http://www.alchemyapi.com/api/ for format of returned object
+      var entities = response.entities;
+      console.log("response ",response.status," how many entities did we get? ", entities.length);    
+    
+      // Do something with data
+      console.log(entities);
+      console.log("you talked about the ",entities[0].type," of ",entities[0].text);
+      
+      io.sockets.emit("newloc", { "address" : entities[0].text });
+    });
+}
 
 io.sockets.on("connection", function(socket) {
     var id = socket.id;
     socket.emit("notice", {
-        msg : "Hello"
+        msg : "Where did you hide the XBox?"
     });
 
     io.sockets.emit("notice", {
@@ -34,10 +53,16 @@ io.sockets.on("connection", function(socket) {
     });
 
     socket.on("chat", function(data) {
+        console.log("received chat data: ", data);
         io.sockets.emit("chat", {
             id: id,
             msg: data
         });
+    });
+    
+    socket.on("getalch", function(data) {
+        console.log("getalch socket received ",data);
+        getAlchemy(data);
     });
 });
 
